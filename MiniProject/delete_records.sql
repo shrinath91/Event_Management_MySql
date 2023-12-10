e86d91cd87b93ff2b104065fff74173d90f57675
shrinath91 <shrinathgarad91@gmail.com>
2023-12-10 21:39:26 +0530

    10-12-2023

---
+++ MiniProject/delete_records.sql
@@ -0,0 +1,10 @@
+truncate table transactions;
+
+truncate table customers;
+
+truncate table artworks;
+
+truncate table artists;
+
+truncate table ngo;
+
---
+++ delete_all_tables.sql
@@ -0,0 +1,5 @@
+truncate table booking;
+truncate table cancel_booking;
+truncate table event;
+truncate table service;
+truncate table user;
